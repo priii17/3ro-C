1092f70f8dae71c66c22df5c0dfa1721b9ebe39b
luu21 <melina09fagundez@gmail.com>
2026-09-03 16:52:31 -0300

    abemus papa

---
+++ inicio.js
@@ -0,0 +1,9 @@
+  
+  const  traslado = document.getElementById('traslado')
+
+ traslado.addEventListener('click', () => {
+ window.location.href='../trabajo/Traslado/registroTraslado.html'
+
+  
+  } )
+
